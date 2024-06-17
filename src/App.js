aea98a6144480675518f034d2acaeb6b6d37782f
andresilva05 <andresilva9657@gmail.com>
2024-06-17 11:35:35 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import People from './assets/people.svg'
+
+import {
+  Container,
+  H1,
+  Image,
+  ContainerItens,
+  InputLabel,
+  Input,
+  Button,
+} from "./styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Image alt="logo-imagem" src={People}/>
+        <ContainerItens>
+      <H1>Olá</H1>
+      <InputLabel>Nome:</InputLabel>
+        <Input placeholder="Nome"/>
+     
+     <InputLabel>Idade:</InputLabel>
+        <Input placeholder="Idade"/>
+        <Button>Cadastrar</Button>
+      </ContainerItens>
+    </Container>
   );
 }
 
-export default App;
+export default App;
