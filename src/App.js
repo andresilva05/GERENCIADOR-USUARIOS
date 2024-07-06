cf84f1416cbea84793142971cdf528d238070562
andresilva05 <andresilva9657@gmail.com>
2024-07-06 18:38:13 -0300

    Alteração nome do site, e troca de background do mesmo.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-import People from './assets/people.svg'
-import Arrow from '.assets/arrow.svg'
+import People from "./assets/people.svg";
+import Arrow from "./assets/arrow.svg";
+import Lixeira from "./assets/lixeira.svg";
 
 import {
   Container,
@@ -11,23 +12,59 @@ import {
   InputLabel,
   Input,
   Button,
+  User,
 } from "./styles";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef();
+  const inputAge = useRef();
+
+  function addNewUser() {
+    setUsers([
+      ...users,
+      {
+        id: Math.random(),
+        name: inputName.current.value,
+        age: inputAge.current.value,
+      },
+    ]);
+  }
+
+  function deleteUser(userId) {
+    const newUsers = users.filter((user) => user.id !== userId);
+
+    setUsers(newUsers)
+    
+  }
+
   return (
     <Container>
-        <Image alt="logo-imagem" src={People}/>
-        <ContainerItens>
-      <H1>Olá</H1>
-      <InputLabel>Nome:</InputLabel>
-        <Input placeholder="Nome"/>
-     
-     <InputLabel>Idade:</InputLabel>
-        <Input placeholder="Idade"/>
-        <Button>Cadastrar <img alt="seta" src={Arrow}/></Button>
+      <Image alt="logo-imagem" src={People} />
+      <ContainerItens>
+        <H1>Olá</H1>
+        <InputLabel>Nome:</InputLabel>
+        <Input ref={inputName} placeholder="Nome" />
+
+        <InputLabel>Idade:</InputLabel>
+        <Input ref={inputAge} placeholder="Idade" />
+        <Button onClick={addNewUser}>
+          Cadastrar <img alt="seta" src={Arrow} />
+        </Button>
+
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p> {user.name}</p> <p> {user.age}</p>
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Lixeira} alt="lata-de-lixo" />
+              </button>
+            </User>
+          ))}
+        </ul>
       </ContainerItens>
     </Container>
   );
 }
 
-export default App;
+export default App;
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import Background from "./assets/background1.svg"
+import Background from "./assets/background.svg"
 
 export const Container = styled.div`
 background:url("${Background}");
@@ -10,10 +10,12 @@ background-size: cover;
     flex-direction: column;
     align-items: center;
     gap: 40px;
+    
 `;
 
 export const Image = styled.img`
     margin-top: 30px;
+    width: 200px;
 `;
 
 export const ContainerItens = styled.div`
@@ -24,6 +26,7 @@ export const ContainerItens = styled.div`
     );
     border-radius:  61px 61px 0px 0px;
     padding: 50px 36px;
+    height: 100vh;
 `;
 
 export const H1 = styled.h1`
@@ -78,5 +81,52 @@ export const Button = styled.button`
     line-height: 28px;
     
     color:#ffffff;
-    cursor: pointer;
+   cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap:20px;
+
+    &:hover{
+        opacity: 0.8;
+    }
+
+    &:active{
+        opacity: 0.5;
+    }
 `;
+
+export const User = styled.li`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 20px;
+
+    background: rgba(255,255,255,0.25);
+    box-shadow: 0px 4px 4px rgba(0,0,0,0.25) ;
+    border-radius: 14px;
+
+    width: 342px;
+    height:58px;
+
+    border: none;
+    outline: none;
+
+    P{
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 28px;
+
+        color: #ffffff;
+    }
+
+    button{
+        background: none;
+        border:none;
+        cursor: pointer;
+
+    }
+
+`
