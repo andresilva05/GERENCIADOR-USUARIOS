8a4eec4c36c90c83bf360b5ddf8466cde949880e
andresilva05 <andresilva9657@gmail.com>
2024-10-03 14:24:16 -0300

    estilização do H1

---
+++ src/components/Title/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  font-size: 32px;
+  color: #333;
+  text-align: center;
+  margin-bottom: 30px;
+
+  @media (max-width: 768px) {
+    font-size: 28px;
+    margin-bottom: 20px;
+  }
+`;
