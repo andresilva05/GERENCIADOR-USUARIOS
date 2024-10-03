1a07e0beafef8ee57eedf4fe072bafac94c79c8b
andresilva05 <andresilva9657@gmail.com>
2024-10-03 14:21:35 -0300

    troca da importação do app para routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import GlobalStyle from "./globalStyle";
+import GlobalStyle from "./styles/globalStyle";
+import Routes from "./routes";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <>
-    <App />
+    <Routes />
     <GlobalStyle />
   </>
 );
